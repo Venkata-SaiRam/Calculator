fa7c6d67dd6a4508236b4318a17a14269e10f592
venkata sai ram <veadala@asu.edu>
2020-02-20 23:04:39 -0700

    Added Calculator

---
+++ Calculator/Assets/ArthematicOperations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class ArthematicOperations : MonoBehaviour
+{
+     public InputField Field1;
+     public InputField Field2;
+     public InputField Result;
+ 
+     public void Sum() {
+         int a = Convert.ToInt32(Field1.text);
+         int b = Convert.ToInt32(Field2.text);
+         int c = a+b;
+         Result.text = c.ToString();
+     }
+ 
+ 
+     public void Product() {
+         int a = Convert.ToInt32(Field1.text);
+         int b = Convert.ToInt32(Field2.text);
+         int c = a*b;
+         Result.text = c.ToString();
+     }
+}
